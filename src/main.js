c32dc7ddc48203a8267ca99e4bbf5daba6f4d470
yevhen <y.kryvoruchko18@gmail.com>
2026-02-05 01:41:14 +0200

    Start of project

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '/components/header/header.js';
+import '/components/hero/hero.js';
+import '/components/artists/artists.js';
+import '/components/artist-modal/artist-modal.js';
+import '/components/layout/layout.js';
+import '/components/feedback/feedback.js';
+import '/components/footer/footer.js';
