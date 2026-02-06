91cef88b87ce41c9e8c74e08597b08bcf418830e
YKryvoruchko <y.kryvoruchko18@gmail.com>
2026-02-06 20:12:21 +0000

    Deploying to gh-pages from @ YKryvoruchko/RhythMix@f3967904a617e37dc6dd264d8e189159d6754eb6 🚀

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const o=document.querySelector("#artists-list");console.log("artistsList:",o);const c={name:"Ren",image:"../../img/ren.png",genres:["Alternative","Pop","Rock","Indie"],description:"Ren Eryn Gill, known professionally as Ren, is a multi-award-winning Welsh singer-songwriter..."};function d(r){return`
-    <div class="artist-card">
-      <div class="artist-card__image">
-        <img src="${r.image}" alt="${r.name}">
-      </div>
-
-      <div class="artist-card__content">
-        <div class="artist-card__genres">
-          ${r.genres.map(s=>`<span class="artist-card__genre">${s}</span>`).join("")}
-        </div>
-
-        <h3 class="artist-card__name">${r.name}</h3>
-
-        <p class="artist-card__description">
-          ${r.description}
-        </p>
-
-        <button class="artist-card__link">
-          Learn More
-          <svg width="8" height="14" viewBox="0 0 8 14" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path d="M0 14L8 7L0 0V14Z" fill="currentColor"/>
-          </svg>
-        </button>
-      </div>
-    </div>
-  `}o.insertAdjacentHTML("beforeend",d(c));
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();document.getElementById("artists-list");
 //# sourceMappingURL=index.js.map
