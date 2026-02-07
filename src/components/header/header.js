923cd8fef3aaa80eab527ca26e21f638a2958b78
Tania Lapa <taniamiroshnuchenko@gmail.com>
2026-02-07 23:38:23 +0100

    update header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,52 @@
 import spriteUrl from '../../img/rhythmix-sprite-symbol-defs.svg?url';
 
-const useEl = document.querySelector('.header-logo-use');
-useEl.setAttribute('href', `${spriteUrl}#icon-logo`);
-useEl.setAttribute('xlink:href', `${spriteUrl}#icon-logo`); 
+const logoUseEls = document.querySelectorAll('.header-logo-use');
+const headerEl = document.querySelector('.header');
+const burgerEl = document.querySelector('.burger');
+const burgerUseEl = document.querySelector('.burger-use');
+const closeEl = document.querySelector('.mobile-close');
+const closeUseEl = document.querySelector('.mobile-close-use');
+const mobileLinks = document.querySelectorAll('.mobile-navigation-link');
+
+logoUseEls.forEach(el => {
+  el.setAttribute('href', `${spriteUrl}#icon-logo`);
+  el.setAttribute('xlink:href', `${spriteUrl}#icon-logo`);
+});
+
+burgerUseEl.setAttribute('href', `${spriteUrl}#icon-menu-alt-right`);
+burgerUseEl.setAttribute('xlink:href', `${spriteUrl}#icon-menu-alt-right`);
+
+closeUseEl.setAttribute('href', `${spriteUrl}#icon-x`);
+closeUseEl.setAttribute('xlink:href', `${spriteUrl}#icon-x`);
 
 
+
+function openMenu() {
+  headerEl.classList.add('is-open');
+  document.body.classList.add('no-scroll');
+  burgerEl.setAttribute('aria-expanded', 'true');
+}
+
+function closeMenu() {
+  headerEl.classList.remove('is-open');
+  document.body.classList.remove('no-scroll');
+  burgerEl.setAttribute('aria-expanded', 'false');
+}
+
+burgerEl.addEventListener('click', e => {
+  e.preventDefault();
+  headerEl.classList.contains('is-open') ? closeMenu() : openMenu();
+});
+
+closeEl.addEventListener('click', e => {
+  e.preventDefault();
+  closeMenu();
+});
+
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => closeMenu());
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') closeMenu();
+});
