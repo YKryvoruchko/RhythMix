5c4e22e0c20d94acbc87790952f6d52e9cfcd039
Tata5773 <124725617+Tata5773@users.noreply.github.com>
2026-02-07 23:46:19 +0100

    Update footer styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,5 @@
 import spriteUrl from '../../img/rhythmix-sprite-symbol-defs.svg?url';
 
 const useEl = document.querySelector('.footer-logo-use');
-useEl.setAttribute('href', `${spriteUrl}#icon-logo`);
-useEl.setAttribute('xlink:href', `${spriteUrl}#icon-logo`);
+useEl.setAttribute('href', `${spriteUrl}#icon-logo-2`);
+useEl.setAttribute('xlink:href', `${spriteUrl}#icon-logo-2`);
