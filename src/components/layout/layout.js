00d2ed33dc7621bc8fdd1e2594b2ef1810b090cc
Jenya Linx <sportpit2020@gmail.com>
2026-02-07 20:07:59 +0200

    change media and add js

---
+++ src/components/layout/layout.js
@@ -0,0 +1,5 @@
+import spriteUrl from '../../img/rhythmix-sprite-symbol-defs.svg?url';
+
+const useEl = document.querySelector('.about-logo-use');
+useEl.setAttribute('href', `${spriteUrl}#icon-logo-icon`);
+useEl.setAttribute('xlink:href', `${spriteUrl}#icon-logo-icon`); 
