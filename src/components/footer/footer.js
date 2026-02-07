1d0e25564494ccb98aa9c5f29cbc733ac4375695
Tata5773 <124725617+Tata5773@users.noreply.github.com>
2026-02-07 23:30:37 +0100

    Resolve merge conflicts

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,5 @@
-const spriteUrl = `${import.meta.env.BASE_URL}img/rhythmix-sprite-symbol-defs.svg`;
+import spriteUrl from '../../img/rhythmix-sprite-symbol-defs.svg?url';
 
 const useEl = document.querySelector('.footer-logo-use');
-
-if (useEl) {
-  useEl.setAttribute('href', `${spriteUrl}#icon-logo`);
-  useEl.setAttribute('xlink:href', `${spriteUrl}#icon-logo`);
-}
-
+useEl.setAttribute('href', `${spriteUrl}#icon-logo`);
+useEl.setAttribute('xlink:href', `${spriteUrl}#icon-logo`);
